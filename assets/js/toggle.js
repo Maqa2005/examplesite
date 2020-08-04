3abe11174992d5f51cf8670f8c72e8f021d72d79
Maqa2005 <45707670+Maqa2005@users.noreply.github.com>
2020-08-04 13:28:03 +0400

    Add files via upload

---
+++ assets/js/toggle.js
@@ -0,0 +1,10 @@
+function toggle() {
+
+    var x = document.getElementById("mobilenav");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+
+}
